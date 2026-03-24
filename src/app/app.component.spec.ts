f20e3f5a4b1de39fb616cd96faf5dd66ffeb5172
Joshua Miller <84877484+joshakaw@users.noreply.github.com>
2026-03-24 14:07:38 -0400

    Correct AppComponent test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,16 +1,36 @@
-import {TestBed, waitForAsync} from '@angular/core/testing';
-import {AppComponent} from './app.component';
-import {TranslateModule} from '@ngx-translate/core';
-import {ElectronService} from './core/services';
-import {provideRouter} from '@angular/router';
+import { TestBed, waitForAsync } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+import { TranslateModule } from '@ngx-translate/core';
+import { ElectronService } from './core/services';
+import { provideRouter } from '@angular/router';
+import { importProvidersFrom } from '@angular/core';
+import { LoggerModule, NgxLoggerLevel } from 'ngx-logger';
+import { DbService } from './core/services/db/db.service';
 
 describe('AppComponent', () => {
   beforeEach(waitForAsync(() => {
+    const mockDbService = {
+      getCashboxAmount: jest.fn(() => {return 100;}),
+    };
+
     void TestBed.configureTestingModule({
       declarations: [],
       imports: [AppComponent, TranslateModule.forRoot()],
-      providers: [provideRouter([]), ElectronService]
+      providers: [
+        provideRouter([]),
+        ElectronService,
+        importProvidersFrom(
+          LoggerModule.forRoot({
+            level: NgxLoggerLevel.TRACE,
+          }),
+        ),
+        { provide: DbService, useValue: mockDbService },
+      ],
     }).compileComponents();
+
+    jest.spyOn(window, 'alert').mockImplementation(function () {
+      console.log('Alert called!');
+    });
   }));
 
   it('should create the app', waitForAsync(() => {
